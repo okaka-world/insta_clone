06acb7390fc4aabb5b44f7ebbd5ea5e40f14a803
EC2 Default User <ec2-user@ip-172-31-46-177.ap-northeast-1.compute.internal>
2020-05-14 13:38:02 +0000

    [Add]ログインログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'users#new'
-  resources :users, only: %i[new create]
+  root "user_sessions#new"
+  resources :users
+  resources :user_sessions, only: %i[create]
+  
+  get "login", to: "user_sessions#new", as: :login
+  delete "logout", to: "user_sessions#destroy", as: :logout
 end
